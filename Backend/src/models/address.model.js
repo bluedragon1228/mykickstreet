fd027e169b2b49427d963769e9156c026ec937d6
bluedragon1228 <joeyharvey066@gmail.com>
2024-03-21 10:27:17 +0530

    added address model

--- Backend/src/models/address.model.js
+++ Backend/src/models/address.model.js
@@ -1,5 +1,33 @@
 const mongoose = require("mongoose")
-const addressSchema = new mongoose.Schema({})
+const addressSchema = new mongoose.Schema({
+    addressName:{
+        type:String,
+    },
+    addressLine1 : {
+        type:String,
+        required : [true,"Address line cannot be empty "]
+    },
+    addressLine2 : {
+        type:String,  
+    },
+    city:{
+        type:String,
+        required : [true,"City cannot be empty"]
+    },
+    state:{
+        type:String,
+        required : [true,"State cannot be empty "]
+    },
+    zipcode:{
+        type:Number,
+        required : [true,"Code line cannot be empty "]
+    },
+    country:{
+        type:String,
+        required : [true,"Country cannot be empty "]
+    },
+    
+})
 
 const address = mongoose.model("address",addressSchema)
 module.exports = address;
