bf1d1b7caa7eb3aec4dd305acc45d52f648ecc05
bluedragon1228 <joeyharvey066@gmail.com>
2024-03-21 19:51:39 +0530

    created routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 const ErrorHandler = require("./utils/errorHandler")
 const userRouter = require("./Routes/User.route")
+const productRouter = require("./Routes/Products.route")
+const orderRouter = require("./Routes/Order.route")
+
 const app = express()
 app.use("/user",userRouter)
-
+app.use('/products',productRouter)
+app.use("/order",orderRouter)
 module.exports = app
--- Backend/src/controllers/customer.js
+++ Backend/src/controllers/customer.js
@@ -1,13 +1,27 @@
 const AsyncHandler = require("../middlewares/AsyncHandler")
 const ErrorHandler = require("../utils/errorHandler")
+const user = require('../models/user.model.js')
+/*
+    User must be able to carry out the following operations
+        Create account
+        Login
+        Check / modify Address
+        Check his orders
 
-
+*/
+// user Router check basic
 const userRouterCheck = AsyncHandler(async(req,res,next)=>{
     res.status(200).json({message:"This is user router",success:true})
 })
-const userLogin = AsyncHandler(async(req,res,next)=>{
 
+// To login 
+const userLogin = AsyncHandler(async(req,res,next)=>{
+    const findUser = await user.find()
+    // console.log(findUser)
+    res.status(200).json({success:true,user:findUser})
 })
+
+// To sign up
 const userSignUp = AsyncHandler(async(req,res,next)=>{
     
 })
