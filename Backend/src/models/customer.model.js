49787a6c5e197c85fc6a9b5848c0dc101edf0657
bluedragon1228 <joeyharvey066@gmail.com>
2024-03-21 10:20:59 +0530

    customer model

--- Backend/src/models/customer.model.js
+++ Backend/src/models/customer.model.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose")
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required : [true,"Enter your name "]
+    },
+    email:{
+        type:String,
+        required : [true,"Enter your email"],
+        unique : true
+    },
+    phone:{
+        type:Number,
+        required : [true,"Enter your phone number"],
+        unique : true
+    },
+    password:{
+        type:String,
+        required : [true,"Password cannot be empty "]
+    },
+    dob:{
+        type:Date
+    }
+})
 
 const user = mongoose.model("user",userSchema)
 module.exports = user;
