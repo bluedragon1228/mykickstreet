d61132a0443fbb14fc069fc9961b27aa9a9bc881
bluedragon1228 <joeyharvey066@gmail.com>
2024-03-22 21:17:51 +0530

    file changes

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const connection = async()=>{
     try{
-        
+       // console.log(process.env)
         await mongoose.connect(`${process.env.URI}/kickStreet`)
         console.log("Connected to data base")
     }catch(err){
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,11 @@
 const connection = require('./db/index.js')
 const app = require("./app.js")
 const Error = require('./middlewares/Error.js')
+require('dotenv').config()
+
 app.use(Error)
 connection().then(()=>{
-    app.listen(4000,()=>console.log("Listening to port ",4000))
+    app.listen(process.env.PORT,()=>console.log("Listening to port ",4000))
 }).catch((err)=>{
     console.log("Connection failed ",err)
 })
