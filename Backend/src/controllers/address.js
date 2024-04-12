3caa99aca6390aed86c3198921c1ff1f95187ac4
bluedragon1228 <joeyharvey066@gmail.com>
2024-04-12 12:14:58 +0530

    Updated address model

---
+++ Backend/src/controllers/address.js
@@ -0,0 +1,15 @@
+const address = require("../models/address.model");
+const AsyncHandler = require("../utils/AsyncHandler");
+const ErrorHandler = require("../utils/errorHandler");
+
+/*
+    Controller to add addresses for a user
+*/
+
+const addAddress = AsyncHandler(async(req,res,next)=>{
+    const {addressName,addressLine1,addressLine2,city,state,zipcode,country} = req.body
+    const {_id} = req.user
+    const user = _id
+    await address.create({user,addressName,addressLine1,addressLine2,city,state,zipcode,country})
+    res.status(200).json({success:true,message:"Address added successfully"})
+})
--- Backend/src/models/address.model.js
+++ Backend/src/models/address.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const addressSchema = new mongoose.Schema({
     user:{
-        type:Schema.Types.ObjectId,
+        type:mongoose.Schema.Types.ObjectId,
         ref:"user"
     },
     addressName:{
