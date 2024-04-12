0b0a865c257d0a2e28bf160563dfc56dd1892267
bluedragon1228 <joeyharvey066@gmail.com>
2024-04-12 11:33:57 +0530

    Added payment contoller

---
+++ Backend/src/controllers/Payment.js
@@ -0,0 +1,8 @@
+const payment = require("../models/payment.model");
+const AsyncHandler = require("../utils/AsyncHandler");
+const ErrorHandler = require("../utils/errorHandler");
+/*
+    Must be able to make a payment 
+    Must be able to change status of payment
+    
+*/
