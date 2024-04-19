1a29af02bf0c749a653e25a94a3e7548bdfded4c
bluedragon1228 <joeyharvey066@gmail.com>
2024-04-19 19:34:16 +0530

    Created navbar and added font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import '@radix-ui/themes/styles.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './global/Navbar';
 function App() {
   return (
-    <>hello</>
+
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          
+          <Route path = "" element=""></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/global/Navbar.tsx
+++ src/global/Navbar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import "../Styles/Navbar.css"
+import logo from "../Assets/Logo.png"
 export default function Navbar() {
   return (
-    <nav>
-      
+    <nav className='border border-slate-950 nav displayFlex'>
+      <div className="displayFlex nav-div">
+        <div className="nav-logo"><img className="logo" src={logo} alt="Logo Here" /></div>
+        <div className="nav-main"><input className="nav-search" type="text" placeholder="Search" /></div>
+        <div className="nav-side">
+          <ul className="flex">
+          <li><i className="fa-regular fa-user px-4"></i></li>
+          <li><i className="fa-solid fa-circle-info"></i></li>
+          <li><i className="fa-solid fa-bag-shopping px-4"></i></li>
+          </ul></div>
+      </div>
     </nav>
   )
 }
