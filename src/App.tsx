f35d065ad5c6e1e013e7ffeb146d250b7ca622d4
bluedragon1228 <joeyharvey066@gmail.com>
2024-04-19 20:30:24 +0530

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import '@radix-ui/themes/styles.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './global/Navbar';
+import Footer from './global/Footer';
 function App() {
   return (
 
@@ -11,6 +12,7 @@ function App() {
           
           <Route path = "" element=""></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   )
--- src/global/Footer.tsx
+++ src/global/Footer.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
-
+import '../Styles/Footer.css'
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <section className='footer-div '>
+      <div className='text-white   my-4 overflow-y-hidden'>
+        <h2 className='text-4xl text-center overflow-y-hidden'>Kick Street</h2>
+      </div>
+      <div>
+        <ul className='flex text-white social  ' >
+          <li><i className="fa-brands fa-x-twitter px-10 overflow-y-hidden"></i></li>
+          <li><i className="fa-brands fa-instagram overflow-y-hidden"></i></li>
+          <li><i className="fa-brands fa-github px-10 overflow-y-hidden"></i></li>
+          <li><i className="fa-brands fa-facebook overflow-y-hidden"></i></li>
+        </ul>
+      </div>
+      <div className='footer-content text-white'>
+        <div>
+          <h2 className='text-sky-400'>Pages</h2>
+          <ul>
+            <li>About</li>
+            <li>Home</li>
+            <li>Cart</li>
+            <li>Top rated</li>
+          </ul>
+        </div>
+        <div>
+        <h2 className='text-sky-400'>Policies</h2>
+          <ul>
+          <li>h</li>
+            <li>h</li>
+            <li>h</li>
+            <li>h</li>
+          </ul>
+        </div>
+        
+      </div>
+      <hr className='my-6'/>
+      <p className='text-center text-white'>Created with  <i className="fa-solid fa-heart text-red-700 text-xl "></i>  by shreyas</p>
+      <p className='text-center font-mono text-orange-300'>2024 github</p>
+      </section>
+    </>
   )
 }
