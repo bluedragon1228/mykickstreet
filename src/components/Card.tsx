15987f673b79ae78a0468e3b740bc2741b0fa6d4
bluedragon1228 <joeyharvey066@gmail.com>
2024-04-21 22:39:09 +0530

    Customized card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,15 @@
+import img from "../Assets/Shoe.webp"
 export default function Card() {
   return (
 <>
-        <div className='border border-slate-950 item-container my-8'> 
-          <div className='h-3/4 border border-slate-950 '></div>
-          <div className=' border h-1/4 border-red-950'> hello</div>
+        <div className=' border-b-2 border-slate-300 item-container my-8'> 
+          <div className='h-3/4 overflow-hidden'>
+            <img src={img} className="object-contain overflow-hidden" alt="" />
+          </div>
+          <div className='  h-1/4  flex items-center flex-col justify-evenly'> 
+            <p className="font-thin">Air Jordan 1 Low 'Bred Toe 2.0'</p>
+            <p >Price : <span className="font-semibold">Rs. 9999</span> </p>
+          </div>
         </div>
 </>
   )
