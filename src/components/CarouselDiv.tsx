ad9339fc994768de608c6663108f1dcd8de9dff2
bluedragon1228 <joeyharvey066@gmail.com>
2024-05-17 18:51:26 +0530

    Code clean up

--- src/components/CarouselDiv.tsx
+++ src/components/CarouselDiv.tsx
@@ -1,29 +1,29 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 import './styles.css';
 
-// import required modules
-import { Navigation } from 'swiper/modules';
+
+import { Autoplay,Pagination } from 'swiper/modules';
+import IntroCard from './Carousel-card/IntroCard';
 export default function CarouselDiv() {
   return (
     <>
-    <div className='my-12' style={{height:"50vh",width:"75%"}}>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide className='text-black' style={{backgroundColor:"red"}}>Slide 1</SwiperSlide>
+    <div className='my-12 rounded' style={{height:"50vh",width:"75%"}}>
+      <Swiper   modules={[Autoplay,Pagination]} className="mySwiper rounded-lg" autoplay={{delay:90000}}>
+        <SwiperSlide className='text-black' style={{backgroundColor:"slategrey"}}><IntroCard/></SwiperSlide>
         <SwiperSlide style={{backgroundColor:"red"}}>wdwdwd</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 3</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 4</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 5</SwiperSlide>
+        <SwiperSlide style={{backgroundColor:"green"}}>Slide 3</SwiperSlide>
+        <SwiperSlide style={{backgroundColor:"yellow"}}>Slide 4</SwiperSlide>
+        <SwiperSlide style={{backgroundColor:"blue"}}>Slide 5</SwiperSlide>
         <SwiperSlide style={{backgroundColor:"red"}}>Slide 6</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 7</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 8</SwiperSlide>
-        <SwiperSlide style={{backgroundColor:"red"}}>Slide 9</SwiperSlide>
+        <SwiperSlide style={{backgroundColor:"pink"}}>Slide 7</SwiperSlide>
+
       </Swiper>
     </div>
     </>
