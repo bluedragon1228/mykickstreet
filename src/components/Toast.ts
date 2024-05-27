cf0cfc2453acfec5f0515b6def0d56aba42aa3cd
bluedragon1228 <joeyharvey066@gmail.com>
2024-05-27 20:05:57 +0530

    Added react toastify

---
+++ src/components/Toast.ts
@@ -0,0 +1,44 @@
+import {toast,Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const toastSuccesss = (message:string)=>{
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+}
+
+export const toastWarning = (message:string)=>{
+    toast.warn(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+}
+
+export const toastError = (message:string)=>{
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
+}
