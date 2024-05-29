7db9421959b9ca979f1079c95b552d2860d5e771
bluedragon1228 <joeyharvey066@gmail.com>
2024-05-29 20:18:08 +0530

    Added reducers

--- src/Redux/Slice/Cart/Index.ts
+++ src/Redux/Slice/Cart/Index.ts
@@ -4,13 +4,18 @@ type Cart = {
     pId:string,
     name:string,
     qty:number,
-    price:number
+    price:number,
+    size:number
 }
-export interface CounterState {
+type PayLoad = {
+  id : string
+  qty : number
+}
+export interface CartState {
   cart: Cart[]
 }
 
-const initialState: CounterState = {
+const initialState: CartState = {
   cart: localStorage.getItem('cart')? JSON.parse(localStorage.getItem('cart')|| " ") : localStorage.setItem('cart',JSON.stringify([]))
 }
 
@@ -22,9 +27,20 @@ export const cartSlice = createSlice({
     addToCart: (state, action: PayloadAction<Cart[]>) => {
       state.cart = action.payload
     },
+    removeFromCart:(state,action: PayloadAction<string>)=>{     
+      state.cart =  state.cart.filter((e)=>e.pId !== action.payload)
+      localStorage.setItem('cart',JSON.stringify(state.cart))
+    },
+    updateQty :({cart},{payload}: PayloadAction<PayLoad>)=>{ 
+        cart.forEach((e)=>{
+          if(e.pId === payload.id)
+            e.qty = payload.qty
+        })
+        localStorage.setItem('cart',JSON.stringify(cart))
+    }
   },
 })
 
-export const {addToCart } = cartSlice.actions
+export const {addToCart,removeFromCart,updateQty } = cartSlice.actions
 
 export default cartSlice.reducer
