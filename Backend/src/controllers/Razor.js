44d25bee2eb95bdfa1005248b1ff84490cc72b86
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-01 23:56:20 +0530

    verifyPayment

--- Backend/src/controllers/Razor.js
+++ Backend/src/controllers/Razor.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const Razorpay = require('razorpay');
+const crypto = require('crypto')
 const AsyncHandler = require("../utils/AsyncHandler");
 const ErrorHandler = require("../utils/errorHandler");
 const instance = new Razorpay({
@@ -27,6 +28,17 @@ const checkOut = AsyncHandler(async(req,res,next)=>{
 
 const verifyPayment = AsyncHandler(async(req,res,next)=>{
     const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.key_secret)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+  if(isAuthentic)
+    return res.status(200).json({success:true,message:"Done payment"})
+  else res.status(400).json({success:false,message:"invalid"})
     res.status(200).json({success:true,message:"Test",body})
 })
 module.exports = {checkOut,verifyPayment}
