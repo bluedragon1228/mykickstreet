9246c6a6fb861b8d128544892cbb79543d5f7c23
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-17 10:56:17 +0530

    code cleanup

--- Backend/src/controllers/order.js
+++ Backend/src/controllers/order.js
@@ -10,6 +10,7 @@ const getAllOrders = AsyncHandler(async(req,res,next)=>{
 const viewOrders = AsyncHandler(async(req,res,next)=>{
     const {_id} = req.user
     const user = _id
+    // const result = await order.find({user}).populate('items.pId').sort('orderDate')
     const result = await order.find({user}).populate('items.pId')
     res.status(200).json({Success:true,result})
 })
