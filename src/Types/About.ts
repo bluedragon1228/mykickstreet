80151e5c565054bf8ab58eb0f0e856928aab7424
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-17 10:56:01 +0530

    Added new type file

---
+++ src/Types/About.ts
@@ -0,0 +1,24 @@
+type ProductId = {
+    images:{url:string}[],
+    name:string,
+    _id : string
+
+  }
+  type Items = {
+    pId:ProductId,
+    price :number,
+    qty : number,
+    size:number
+  }
+  export type Data = {
+    success:boolean,
+    result : Result[]
+  }
+  export type Result = {
+    amount:number, 
+    items:Items[],
+    orderDate:string,
+    payment : string,
+    user : string,
+    _id : string
+  }
