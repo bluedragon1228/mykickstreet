97824fa33787d0b6e113d80a24deafe38293ea6c
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-21 22:15:50 +0530

    Modal

---
+++ src/components/Modal/ModalWrapper.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ModalWrapper() {
+  return (
+    <>
+     <section className=' z-50 absolute top-0 bottom-0 right-0 left-0 opacity-90 bg-slate-800 flex justify-center items-center overflow-hidden' style={{height:"100vh",width:"100vw"}}>
+            <form className='w-2/3 h-2/3 bg-black rounded'>
+            
+            </form>
+        
+    </section> 
+    </>
+  )
+}
--- src/page/Admin/PaymentAdmin.tsx
+++ src/page/Admin/PaymentAdmin.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ModalWrapper from '../../components/Modal/ModalWrapper'
 
 export default function PaymentAdmin() {
   return (
     <>
-     <section className='adminPage bg-white p-2'></section> 
+    <ModalWrapper/>
     </>
   )
 }
