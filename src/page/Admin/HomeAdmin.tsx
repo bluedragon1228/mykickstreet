6dbd86805a91eeb2ed3d989e71a9ee6023271dde
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-23 23:16:29 +0530

    fetch stats

--- src/page/Admin/HomeAdmin.tsx
+++ src/page/Admin/HomeAdmin.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LineChart from '../../components/Charts/LineChart'
 import BarChart from '../../components/Charts/BarChart'
-
+type Result ={
+  orderAmount:number,
+  orderCount:number,
+  productCount:number
+}
+type Data = {
+  success :boolean,
+  result:Result
+}
 export default function HomeAdmin() {
+
+  const [stats,setStats] = useState<Result>()
+  const getData = async()=>{
+    try{
+      const response = await fetch('http://localhost:4000/admin/stats', {
+        method: "GET", 
+        mode: "cors", 
+        credentials: "include", 
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data:Data = await response.json();
+      console.log(data)
+      setStats(data.result) 
+     
+    
+    }catch(e){
+      console.log(e)
+    }
+  }
+  useEffect(()=>{getData()},[])
   return (
     <>
      <section className='adminPage bg-white ' >
@@ -19,12 +49,25 @@ export default function HomeAdmin() {
       <div className='w-full h-auto min-h-96 '>
         <div className='flex items-center justify-evenly py-5'>
           <div className='w-1/2 flex justify-around'>
-            <div className='w-1/3 h-40 border'> Total revenue</div>
-            <div className='w-1/3 h-40 border'>Total Products</div>
+            <div className='w-1/3 h-40 border flex flex-col justify-around '> 
+                <p className='capitalize font-bold text-indigo-700 text-xl pl-5'><i className="fa-solid fa-indian-rupee-sign mx-2 bg-indigo-700 text-white py-2 px-3 rounded"> </i>Total revenue</p>
+                <p className='text-3xl text-gray-500 pb-5 text-center'>₹{stats?.orderAmount.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+
+            </div>
+            <div className='w-1/3 h-40 border flex flex-col justify-around '>
+            <p className='capitalize font-bold text-indigo-700 text-xl pl-5'><i className="fa-solid fa-box mx-2 bg-indigo-700 text-white py-2 px-3 rounded"></i>Total products</p>
+            <p className='text-3xl text-gray-500 pb-5 text-center'>{stats?.productCount}</p>
+            </div>
           </div>
           <div className='w-1/2 flex justify-around'>
-            <div className='w-1/3 h-40 border'>Total orders </div>
-            <div className='w-1/3 h-40 border'></div>
+            <div className='w-1/3 h-40 border flex flex-col justify-around '>
+            <p className='capitalize font-bold text-indigo-700 text-xl pl-5'><i className="fa-solid fa-boxes-stacked mx-2 bg-indigo-700 text-white py-2 px-3 rounded"></i>Total orders</p>
+            <p className='text-3xl text-gray-500 pb-5 text-center'>{stats?.orderCount}</p>
+             </div>
+            <div className='w-1/3 h-40 border flex flex-col justify-around '>
+            <p className='capitalize font-bold text-indigo-700 text-xl pl-5'>Total revenue</p>
+            <p className='text-3xl text-gray-500 pb-5 text-center'>{stats?.orderAmount.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+            </div>
           </div>
         </div>
         <div className='flex items-center justify-around '>
