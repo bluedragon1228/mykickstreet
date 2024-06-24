b5f9091585e947ac6bb9cb72b7cf8fa1b42280ab
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-24 15:13:36 +0530

    Chart functionality

--- src/components/Charts/BarChart.tsx
+++ src/components/Charts/BarChart.tsx
@@ -2,20 +2,28 @@ import React from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Chart, CategoryScale, BarElement, LinearScale, PointElement, LineElement } from "chart.js";
 import { Bar, Line } from "react-chartjs-2";
 ChartJS.register(BarElement,Tooltip,Legend,CategoryScale,LinearScale,PointElement,LineElement)
-const labels = ["January", "February", "March", "April", "May", "June"];
-const data = {
-  labels: labels,
-  datasets: [
-  {
-  label: "My First dataset",
-  backgroundColor: "red", 
-  borderColor: "rgb(255, 99, 132)",
-  data: [0, 10, 5, 2, 20, 30, 45],
-  },
-  ],
-  };
+//const labels = ["January", "February", "March", "April", "May", "June"];
 
-export default function BarChart() {
+  type Props = {
+    month:string[],
+  orders:number[],
+
+  }
+export default function BarChart({month,orders}:Props) {
+  const data = {
+    labels: month,
+    datasets: [
+    {
+    label: "Orders By Month",
+    backgroundColor: "rgb(67 56 202)", 
+    borderColor: "#5c64ff",
+    barThickness : 25,
+    borderRadius:10,
+  
+     data: orders,
+    },
+    ],
+    };
   return (
     <div className='w-5/6 h-auto border min-h-60'>
       <Bar data={data} />
--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Chart, CategoryScale, BarElement, LinearScale, PointElement, LineElement } from "chart.js";
 import { Bar, Line } from "react-chartjs-2";
 ChartJS.register(BarElement,Tooltip,Legend,CategoryScale,LinearScale,PointElement,LineElement)
-const labels = ["January", "February", "March", "April", "May", "June"];
-const data = {
-  labels: labels,
-  datasets: [
-  {
-  label: "My First dataset",
-  backgroundColor: "red", 
-  borderColor: "rgb(255, 99, 132)",
-  data: [0, 10, 5, 2, 20, 30, 45],
-  },
-  ],
-  };
-export default function LineChart() {
+//const labels = ["January", "February", "March", "April", "May", "June"];
+
+  type Props = {
+    month:string[],
+
+  revenue:number[]
+  }
+export default function LineChart({month,revenue}:Props) {
+  const data = {
+    labels: month,
+    datasets: [
+    {
+    label: "Revenue By Month",
+    backgroundColor: "rgb(67 56 202)", 
+    borderColor: "rgb(67 56 202)",
+    data: revenue,
+    tension: 10,
+    pointRadius:6
+    },
+    ],
+    };
   return (
     <div className='w-5/6 h-auto border min-h-60'>
        <Line data={data}  />
