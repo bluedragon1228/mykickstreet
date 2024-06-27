282a71b71f7f26e6e3c3135a262b51e6a7992e00
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-27 11:31:20 +0530

    code cleanup

--- src/components/Modal/ModalWrapper.tsx
+++ src/components/Modal/ModalWrapper.tsx
@@ -9,7 +9,7 @@ type Data = {
   result : Product
 }
 export default function ModalWrapper({setShow,productId}:Props) {
-  const [details,setDetails] = useState({name:'',gender:'',description:'',price:0,discount:0})
+  const [details,setDetails] = useState({name:'',gender:'',description:'',price:0,discount:0,stock:0})
   const [size,setSize] = useState<Size[]>()
   const closeModal = ()=>{
     document.body.style.overflow = "scroll"
@@ -29,14 +29,16 @@ export default function ModalWrapper({setShow,productId}:Props) {
       const data:Data = await response.json();
       const {result }= data
       console.log(data) 
-      setDetails({description:result.description,gender:result.gender,name:result.name,price:result.price,discount:result.offer})
-      setSize(result.size)
-      //setDetails(data.result)
-     
-    
+      setDetails({description:result.description,gender:result.gender,name:result.name,price:result.price,discount:result.offer,stock:result.stock})
+      setSize(result.size)  
     }catch(e){
       console.log(e)
     }
+  }
+
+  const handleSubmit = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+    e.preventDefault()
+    console.log(details)
   }
 
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
@@ -47,7 +49,7 @@ export default function ModalWrapper({setShow,productId}:Props) {
     console.log(details)
   }
   useEffect(()=>{
-    getData()
+    productId && getData() 
   },[])
   return (
     <>
@@ -56,8 +58,8 @@ export default function ModalWrapper({setShow,productId}:Props) {
               <div className='w-full flex flex-row-reverse pr-5 '><button onClick={closeModal} className='border-black m-3 font-thin text-6xl'>&times;</button></div>
                 <form className='h-5/6  w-full pt-5 '>
                 <div className=' w-full h-16 flex flex-row-reverse py-2 font-medium'>
-                <button className='py-2 px-8 border mx-5 bg-green-400 rounded hover:bg-green-500'>Update</button>
-                  <button  className='py-2 px-8 border bg-red-500 rounded hover:bg-red-600'>Delete</button>
+                <button className='py-2 px-8 border mx-5 bg-green-400 rounded hover:bg-green-500' onClick={handleSubmit}>{productId? 'Update' : 'Add'}</button>
+                  {productId && <button  className='py-2 px-8 border bg-red-500 rounded hover:bg-red-600'>Delete</button>}
                  
                 </div>
                     <div className='w-full h-full flex'>
@@ -72,11 +74,6 @@ export default function ModalWrapper({setShow,productId}:Props) {
                               <br />
                               <input type="text" className='border outline-none py-2 w-4/5 px-2 rounded border-slate-300'  />
                             </div>
-                            {/* <div className='my-3'>
-                              <label className='p-1 font-bold text-zinc-700 text-sm'>Product Name</label>
-                              <br />
-                              <input type="text" className='border outline-none py-2 w-4/5 px-2 rounded border-slate-300' />
-                            </div> */}
                             <div className='my-3'>
                               <label className='p-1 font-bold text-zinc-700 text-sm'>Gender</label>
                               <br />
@@ -115,22 +112,22 @@ export default function ModalWrapper({setShow,productId}:Props) {
                             <div className='mx-2'>
                             <label htmlFor=""><span className='p-1 font-bold text-zinc-700 text-sm'>Discount</span> <span className='text-gray-600 text-sm'>(Optional)</span></label>
                             <br />
-                            <input type="text" className='px-2 py-1 outline-none border border-slate-300 rounded' value={details.discount}/>
+                            <input type="text" className='px-2 py-1 outline-none border border-slate-300 rounded'name='discount' onChange={handleChange} value={details.discount}/>
                             </div>
                             <div className='mx-2'>
                             <label className='p-1 font-bold text-zinc-700 text-sm'>Discount Price</label>
                             <br />
-                            <input type="text" className='px-2 py-1 outline-none border bg-white border-slate-300 rounded' disabled value={ details.price - (details.price * details.discount)}/>
+                            <input type="text" className='px-2 py-1 outline-none border bg-white border-slate-300 rounded' disabled value={ details.price - (details.price * details.discount/100)}/>
                             </div>
                            </div>
-                           <h2 className='my-3 px-1 text-lg font-bold text-zinc-700'>Stock</h2>
+                           <h2 className='my-3 px-1 text-lg font-bold text-zinc-700'>Stock ({details.stock})</h2>
                            <div className='flex flex-wrap  border border-slate-200 mr-3'>
                                 {size?.map(e=>
                                    <div className='w-24 h-16 p-1 border border-dashed bg-zinc-100 rounded m-2'>
                                     <p className='text-center'>UK : {e.size}</p>
                                     <input type="number" className=' text-center w-full px-2 outline-none rounded border py-1' value={e.stock} />
                                 </div>)}    
-                                <div className='w-24 h-16 p-1 border border-dashed bg-zinc-100 rounded m-2'> <button className='w-full h-full  text-sm text-violet-700'>Add more +</button> </div>                            
+                                <div className='w-24 h-16 p-1 border border-dashed bg-zinc-100 rounded m-2'> <button className='w-full h-full  text-sm text-violet-700'>{productId? 'Add more +' : 'Add size'}</button> </div>                            
                            </div>
                         </div>  
                         </div>     
