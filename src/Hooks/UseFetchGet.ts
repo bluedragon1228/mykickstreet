f8851677db208f4492eabe02c35b397914d2d87c
bluedragon1228 <joeyharvey066@gmail.com>
2024-06-28 00:01:19 +0530

    Custom hook set up

--- src/Hooks/UseFetchGet.ts
+++ src/Hooks/UseFetchGet.ts
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function UseFetchGet<T>(url:string):[T|undefined,boolean] {
+export default function UseFetchGet<T>(url:string,reroute:string):[T|undefined,boolean] {
     const [loading,setLoading] = useState<boolean>(true)
     const [data,setData] = useState<T>()
     const navigate = useNavigate()
     const getData = async()=>{
+        try{
+          const response = await fetch(url,{
+            method: "GET", 
+              mode: "cors", 
+              credentials: "include", 
+              headers: {
+                "Content-Type": "application/json",
+              },
+          })
+          if(response.status === 500)
+              return navigate(reroute)
+          const data = await response.json()
+          //console.log(data)
+        //   setOrders(data.orders)
+        //   setDetails(data.userDetails[0])
+        setData(data.result)
 
+        }catch(e){}
     }
 
     useEffect(()=>{
--- src/Hooks/UseFetchPost.ts
+++ src/Hooks/UseFetchPost.ts
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function UseFetchPost<T>(url:string,body:T):[T|undefined,boolean] {
+export default function UseFetchPost<T,U>(url:string,body:U):(T|undefined) {
     const [loading,setLoading] = useState<boolean>(true)
-    const [data,setData] = useState<T>()
+    const [data,setData] = useState<T>()!
     const navigate = useNavigate()
     const getData = async()=>{
-
+      try{
+        const response = await fetch(url, {
+          method: "POST", 
+          mode: "cors", 
+          credentials: "include", 
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body)
+        });
+        const data = await response.json()!;
+        //console.log(data) 
+        // data.result ? setData(data.result) : setData()
+        
+          setData(data.result)
+        
+        //setDetails({description:result.description,gender:result.gender,name:result.name,price:result.price,discount:result.offer,stock:result.stock})
+        //setSize(result.size)  
+      }catch(e){
+        console.log(e)
+      }
     }
 
     useEffect(()=>{
         getData()
     },[url])
 
-  return [data,loading]
+  return data
 }
