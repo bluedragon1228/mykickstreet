ce950b5648e12966a5fcfad29d07a4db33316d27
bluedragon1228 <joeyharvey066@gmail.com>
2024-07-06 22:07:20 +0530

    Slight change in code

--- Backend/src/middlewares/Auth.customer.js
+++ Backend/src/middlewares/Auth.customer.js
@@ -9,12 +9,12 @@ const userAuth = async(req,res,next)=>{
     jwt.verify(Auth,process.env.JWTPASSWORD,async(err,user)=>{
         if(err){
             res.clearCookie("Auth")
-            return next(new ErrorHandler("Invalid token, login again",402))
+            return next(new ErrorHandler("Invalid token, login again",401))
         }
            
         const check = await User.findOne({email:user.email})
         if(!check || check.type !== "user")
-            return next(new ErrorHandler("Invalid token, login again",402))
+            return next(new ErrorHandler("Invalid token, need to login as a user",401))
         if(check.type !== "user")
             return next(new ErrorHandler("Not a user",402))
         req.user = user
