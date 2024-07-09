dc72853b7c1e63552601da1d9e0826102391aa97
bluedragon1228 <joeyharvey066@gmail.com>
2024-07-09 22:39:50 +0530

    Test

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 type Data = {
   total :number,
   skip:number,
@@ -11,11 +11,40 @@ type Body = {
 }
 export default function About() {
   // const [data,loading] = UseFetch<Data>('https://dummyjson.com/products')
+  const [image,setImage] = useState<File >()
   // UseEffectGet<Body>('http://localhost:4000/user/logon',{userEmail:"shreyas.hrithvik@gmail.com",password:'123456789'})
   // console.log("data is",data)
+  const handleSubmit = async(e:React.FormEvent<HTMLFormElement>)=>{{
+    e.preventDefault()
+    const form = new FormData()
+    if(image){
+      form.append("image",image)
+      try{
+        const response = await fetch('http://localhost:4000/products/upload',{
+            method:"POST",
+            mode: "cors", 
+        credentials: "include", 
+          body:form
+        })
+        if(response.ok)
+          console.log('Uploaded')
+
+      }catch(e){}
+    }
+      
+  }}
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    console.log(e.target.files?.[0])
+    setImage(e.target.files?.[0])
+  }
   return (
     <>
     <section className='page '>
+    <form action="/profile" encType="multipart/form-data" onSubmit={handleSubmit}>
+        <input type="file" name="avatar" onChange={handleChange}/>
+      <button >Submit</button>
+    </form>
+
     </section> 
     </>
   )
