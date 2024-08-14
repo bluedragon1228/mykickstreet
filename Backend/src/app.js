6fe931b312db90b1ce948e61944ffd50264484a5
bluedragon1228 <joeyharvey066@gmail.com>
2024-08-14 12:23:02 +0530

    added logout functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,6 +8,7 @@ const categoryRouter = require("./Routes/Category.route")
 const adminRoute = require("./Routes/Admin.route")
 const paymentRouter = require("./Routes/Payment.route")
 const cors = require('cors')
+const AsyncHandler = require("./utils/AsyncHandler")
 const app = express()
 app.use(cors({origin:true,credentials: true}))
 app.use(express.json())
@@ -19,6 +20,10 @@ app.use("/order",orderRouter)
 app.use('/category',categoryRouter)
 app.use('/admin',adminRoute)
 app.use('/payment',paymentRouter)
+app.get('/logout',AsyncHandler(async(req,res,next)=>{
+    res.clearCookie("Auth")
+    res.status(200).json({success:true,message:"Logged out successfully"})
+}))
 app.use(APIError)
 module.exports = app
 
